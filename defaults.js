11f876d8eabbcad19ef6b47c6bf64a5eec363ae8
Hashir <mostwantedhashir+1@gmail.com>
2024-12-23 21:33:10 +0500

    Create defaults.js

---
+++ defaults.js
@@ -0,0 +1,25 @@
+{
+	"generate": {
+		"prompt": "",
+		"negativePrompt": "", 
+		"model": null,
+		"seed": -1, 
+		"initImage": null, 
+		"denoising": 0.55, 
+		"subseed": -1, 
+		"subseedStrength": 0.0, 
+		"steps": 30, 
+		"cfg": 9,
+		"width": 512,
+		"height": 512,
+		"sampler": "DPM++ 2M",
+		"hrScale": 1,
+		"latentSpace": false,
+		"clipSkip": 2,
+		"batchSize": 1
+	},
+	"hiresFixModal": {
+		"hrScale": "1.5",
+		"denoising": "0.55"
+	}
+}
